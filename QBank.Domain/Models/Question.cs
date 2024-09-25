d787133e3c95bbde902965907d503ac627fe395c
Lucca-Fernandes <lfdc212@gmail.com>
2024-09-25 10:58:10 -0300

    Adiciona estrutura básica do projeto e classe de modelo Question

---
+++ QBank.Domain/Models/Question.cs
@@ -0,0 +1,7 @@
+public class Question
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string[] Options { get; set; }
+    public string CorrectAnswer { get; set; }
+}
